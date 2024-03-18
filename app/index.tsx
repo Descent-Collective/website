db9dc9017c591a848499a45725bd96751dc26a6e
meisterjustice <eziefulejustice@gmail.com>
2024-03-18 06:06:42 +0100

    add content

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,7 @@
-"use client";
-import { DescentFooter } from "@/components";
-import DescentHeader from "@/components/header";
-
 import { ReactNode } from "react";
 
 const AppHome = ({ children }: { children: ReactNode }) => {
-  return (
-    <main className="font-SF_UI_Display">
-      <div>{children}</div>
-    </main>
-  );
+  return <main>{children}</main>;
 };
 
 export default AppHome;
--- views/home/index.tsx
+++ views/home/index.tsx
@@ -1,5 +1,10 @@
-import { DescentContainer, DescentHeader } from "@/components";
+import {
+  DescentClickAnimation,
+  DescentContainer,
+  DescentHeader,
+} from "@/components";
 import { LogoIcon } from "@/public/icons";
+import Link from "next/link";
 
 const HomeView = () => {
   return (
@@ -12,6 +17,35 @@ const HomeView = () => {
 
       <div className="relative z-50">
         <DescentHeader />
+
+        <div className="mt-[40%] md:mt-[10%] flex flex-col items-center justify-center gap-5">
+          <div className="border border-grey-1050 rounded-full h-[30px] px-3 flex items-center bg-white-50">
+            <div className="font-medium text-sm md:text-base text-grey-1000">
+              Descent Protocol
+            </div>
+          </div>
+
+          <div className="md:w-[80%] xl:w-[60%] flex justify-center">
+            <h1 className="text-4xl md:text-6xl xl:text-[64px] md:leading-[64px] font-semibold text-grey-1100 text-center">
+              A micropayment stablecoin for the onchain economy
+            </h1>
+          </div>
+
+          <div className="text-center md:text-xl text-grey-1200">
+            Powering finance in the Farcaster-driven ecosystem
+          </div>
+
+          <DescentClickAnimation>
+            <Link
+              href="#"
+              className="rounded-full h-[40px] px-4 flex items-center bg-green-650"
+            >
+              <div className="font-medium text-base md:text-lg text-white-50">
+                Explore frames
+              </div>
+            </Link>
+          </DescentClickAnimation>
+        </div>
       </div>
     </div>
   );
